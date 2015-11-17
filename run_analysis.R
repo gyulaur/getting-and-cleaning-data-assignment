1ef29aa364027ecfcb0c37366f8a4671da8e8218
Gyula Magyar <gyula@gyulaur.com>
2015-11-17 20:02:36 +0100

    fix validation

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,21 @@
 # author: Gyula Magyar for the coursera course "Getting and Cleaning Data"
 
 run_analysis <- function() {
+  # Basic validation to ensure that the required input files are in place
   validateFiles()
+  
+  # Read the required files
+  activity_labels <- read.table("UCI_HAR_Dataset/activity_labels.txt", col.names=c("activity_id", "activity_name"))
+  feature_names <- read.table("UCI_HAR_Dataset/features.txt")[,2]
+  subjects_test <- read.table("UCI_HAR_Dataset/test/subject_test.txt")
+  
 }
 
 validateFiles <- function() {
-  required_files <- c("UCI_HAR_Dataset/activity_labels.txt", "UCI_HAR_Dataset/features.txt",
-                      "UCI_HAR_Dataset/test/subject_test.txt", "UCI_HAR_Dataset/test/X_test.txt",
-                      "UCI_HAR_Dataset/test/y_test.txt", "UCI_HAR_Dataset/train/subject_test.txt",
-                      "UCI_HAR_Dataset/train/X_test.txt", "UCI_HAR_Dataset/train/y_test.txt")
-  if (!file.exists("UCI_HAR_Dataset/activity_labels.txt")) {
+  if (!file.exists("UCI_HAR_Dataset/activity_labels.txt") || !file.exists("UCI_HAR_Dataset/features.txt")
+      || !file.exists("UCI_HAR_Dataset/test/subject_test.txt") || !file.exists("UCI_HAR_Dataset/test/X_test.txt")
+      || !file.exists("UCI_HAR_Dataset/test/y_test.txt") || !file.exists("UCI_HAR_Dataset/train/subject_train.txt")
+      || !file.exists("UCI_HAR_Dataset/train/X_train.txt") || !file.exists("UCI_HAR_Dataset/train/y_train.txt")) {
     stop("Missing input files")
   }
 }
