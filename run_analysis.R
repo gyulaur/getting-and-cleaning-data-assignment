a3c05452490a5593d017fc3bb258eaa605cb91d3
Gyula Magyar <gyula@gyulaur.com>
2015-11-21 16:15:11 +0100

    Remove some duplications

--- run_analysis.R
+++ run_analysis.R
@@ -3,25 +3,35 @@
 library(dplyr)
 library(reshape2)
 
+# Creating variables for the filenames to avoid some duplication
+activities_file <- "UCI_HAR_Dataset/activity_labels.txt"
+features_file <- "UCI_HAR_Dataset/features.txt"
+test_subjects_file <- "UCI_HAR_Dataset/test/subject_test.txt"
+test_set_file <- "UCI_HAR_Dataset/test/X_test.txt"
+test_labels_file <- "UCI_HAR_Dataset/test/y_test.txt"
+train_subjects_file <- "UCI_HAR_Dataset/train/subject_train.txt"
+train_set_file <- "UCI_HAR_Dataset/train/X_train.txt"
+train_labels_file <- "UCI_HAR_Dataset/train/y_train.txt"
+
 run_analysis <- function() {
   # Basic validation to ensure that the required input files are in place
   validateFiles()
   
   # Reading common input files
-  activities <- read.table("UCI_HAR_Dataset/activity_labels.txt", col.names=c("activityId", "activityName"))
-  features <- read.table("UCI_HAR_Dataset/features.txt", col.names=c("featureId", "featureName"))
+  activities <- read.table(activities_file, col.names=c("activityId", "activityName"))
+  features <- read.table(features_file, col.names=c("featureId", "featureName"))
   # Selecting the required features
   extractedFeatures <- grep("-mean\\(\\)|-std\\(\\)", features$featureName)
   
   # Reading the test data set
-  test_subjects <- read.table("UCI_HAR_Dataset/test/subject_test.txt")
-  test_set <- read.table("UCI_HAR_Dataset/test/X_test.txt")
-  test_labels <- read.table("UCI_HAR_Dataset/test/y_test.txt")
+  test_subjects <- read.table(test_subjects_file)
+  test_set <- read.table(test_set_file)
+  test_labels <- read.table(test_labels_file)
   
   # Reading the training data set
-  train_subjects <- read.table("UCI_HAR_Dataset/train/subject_train.txt")
-  train_set <- read.table("UCI_HAR_Dataset/train/X_train.txt")
-  train_labels <- read.table("UCI_HAR_Dataset/train/y_train.txt")
+  train_subjects <- read.table(train_subjects_file)
+  train_set <- read.table(train_set_file)
+  train_labels <- read.table(train_labels_file)
   
   # Merging the training and test sets to one data set (step 1)
   X <- rbind(train_set, test_set)
@@ -56,10 +66,10 @@ run_analysis <- function() {
 }
 
 validateFiles <- function() {
-  if (!file.exists("UCI_HAR_Dataset/activity_labels.txt") || !file.exists("UCI_HAR_Dataset/features.txt")
-      || !file.exists("UCI_HAR_Dataset/test/subject_test.txt") || !file.exists("UCI_HAR_Dataset/test/X_test.txt")
-      || !file.exists("UCI_HAR_Dataset/test/y_test.txt") || !file.exists("UCI_HAR_Dataset/train/subject_train.txt")
-      || !file.exists("UCI_HAR_Dataset/train/X_train.txt") || !file.exists("UCI_HAR_Dataset/train/y_train.txt")) {
+  if (!file.exists(activities_file) || !file.exists(features_file)
+      || !file.exists(test_subjects_file) || !file.exists(test_set_file)
+      || !file.exists(test_labels_file) || !file.exists(train_subjects_file)
+      || !file.exists(train_set_file) || !file.exists(train_labels_file)) {
     stop("Missing input files")
   }
 }
